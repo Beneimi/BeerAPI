f0cdc1ba43a7f2ddf836cfac6f35286116a487b5
Bene ImI <beneimi99@gmail.com>
2020-12-19 22:31:39 +0000

    Enable CORS

--- src/main/java/com/beneimi/BeerController.java
+++ src/main/java/com/beneimi/BeerController.java
@@ -17,36 +17,42 @@ public class BeerController {
     //
 
     // All
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers")
     List<Beer> findAll() {
         return repository.findAll();
     }
 
     // By Id
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers/id={id}")
     List<Beer> findOne(@PathVariable Long id) {
         return repository.findAllById(id);
     }
 
     // By Name
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers/name={name}")
     List<Beer> findByName(@PathVariable String name) {
         return repository.findByNameOrderByName(name);
     }
 
     // By Type
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers/type={type}")
     List<Beer> findByType(@PathVariable String type) {
         return repository.findByTypeOrderByName(type);
     }
 
     // By Brewery
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers/brewery={brewery}")
     List<Beer> findByBrewery(@PathVariable String brewery) {
         return repository.findByBreweryOrderByName(brewery);
     }
 
     // By Rating
+    @CrossOrigin(origins = "http://localhost:3000")
     @GetMapping("/beers/rating={rating}")
     List<Beer> findRatingHigher(@PathVariable Double rating) {
         return repository.findByRatingGreaterThanOrderByRating(rating);
@@ -58,6 +64,7 @@ public class BeerController {
     //
 
     // Create
+    @CrossOrigin(origins = "http://localhost:3000")
     @PostMapping("/beers")
     @ResponseStatus(HttpStatus.CREATED)
     Beer newBeer(@RequestBody Beer newBeer) {
@@ -66,6 +73,7 @@ public class BeerController {
 
 
     // Create with id / Update
+    @CrossOrigin(origins = "http://localhost:3000")
     @PutMapping("/beers/id={id}")
     Beer saveOrUpdate(@RequestBody Beer newBeer, @PathVariable Long id) {
         return repository.findById(id)
@@ -87,12 +95,14 @@ public class BeerController {
     //
 
     // All
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/beers")
     void deleteAll() {
         repository.deleteAll();
     }
 
     // By Id
+    @CrossOrigin(origins = "http://localhost:3000")
     @DeleteMapping("/beers/id={id}")
     void deleteBeer(@PathVariable Long id) {
         repository.deleteById(id);
